cdc8a0b87a9579cc6018298cca2b11666495783d
V3RSER <luisdeiko@outlook.com>
2022-03-18 11:11:26 -0500

    Filtro ranking

--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.dtos.PlayerDTO;
+import com.example.demo.models.Player;
 import com.example.demo.utils.CsvUtilFile;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class CSVUtilTest {
 
@@ -48,5 +46,20 @@ public class CSVUtilTest {
         listFilter.subscribe(players -> System.out.println("Jugadores del club " + club + ": " + players.size()));
         listFilter.block().forEach((name, playerDTOS) -> playerDTOS.forEach(System.out::println));
         assert listFilter.block().size() == 33;
+    }
+
+    @Test
+    void stream_filterPlayersRankingByNationaly() {
+        var nationality = "France";
+        List<PlayerDTO> list = CsvUtilFile.getPlayers();
+        Flux<PlayerDTO> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<PlayerDTO>>> listFilter = listFlux
+                .filter(playerDTO -> playerDTO.getNationality().equals(nationality))
+                .sort(Comparator.comparing(PlayerDTO::getWinners).reversed())
+                .distinct()
+                .collectMultimap(PlayerDTO::getName);
+        listFilter.subscribe(players -> System.out.println("Jugadores con nacionalidad " + nationality + ": " + players.size()));
+        listFilter.block().forEach((name, playerDTOS) -> playerDTOS.forEach(System.out::println));
+        assert listFilter.block().size() == 904;
     }
 }
